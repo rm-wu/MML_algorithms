bef627cf4b942fe3a619777e48aa7be195f6fd86
Riccardo Mereu <riccardomereu5@gmail.com>
2020-07-19 22:32:49 +0200

    modified PCA fit method

--- MML_algorithms/dimensionality_reduction/PCA.py
+++ MML_algorithms/dimensionality_reduction/PCA.py
@@ -6,7 +6,7 @@ from matplotlib import pyplot as plt
 from ..utils import flip_svd, check_data
 
 class PCA:
-    def __init__(self, num_components=2, centering_data=True, verbose=False):
+    def __init__(self, num_components=2, verbose=False):
         '''
 
         :param num_components: int, default=2
@@ -29,9 +29,8 @@ class PCA:
         self.means = None
 
         self.principal_components = None
-        self.singular_values = None
+        #self.singular_values = None
 
-        self.centering_data = centering_data
 
     def fit(self, X):
         '''
@@ -44,98 +43,51 @@ class PCA:
         if self.verbose:
             print("Begin PCA")
             print("-" * 50)
-            print(f"Data matrix X dimensions : {self.num_samples} samples, {self.num_features} features")
+            print(f"Data matrix dimensions : {self.num_samples} samples, {self.num_features} features")
             print("-" * 50)
 
-        if self.centering_data:
-            self.means = X.mean(axis=0)
-            X_ = X - self.means
-            if self.verbose:
-                print("Centering data matrix")
-                print(" - computing X columns means")
-                print(" - subtract the mean from the original data matrix")
-                print("-" * 50)
-        else:
-            X_ = X.copy()
+        self.means = X.mean(axis=0)
+        X_ = X - self.means
+        if self.verbose:
+            print("Centering data matrix")
+            print(" - computing X columns means")
+            print(" - subtract the mean from the original data matrix")
+            print("-" * 50)
+
         singular_values_ = None
 
-        if X.shape[0] > X.shape[1]:
-            #TODO: change it to (not shape1 >>> shape0)
-            # case A
-            A = (X_.T @ X_) / (self.num_samples - 1)
-            if self.verbose:
-                print("Case A (num_samples > num_features) :")
-                print(f"- Compute A = X'X")
-                print(f"\t- X'X shape : ({X_.shape[1]} x {X_.shape[0]}) * ({X_.shape[0]} x {X_.shape[1]})")
-                print(f"\t            :  {A.shape[0]} x {A.shape[1]}")
-                print("-" * 50)
+        U, S, Vh = np.linalg.svd(X_, full_matrices=False)
+        # to obtain a deterministic result flip the sign of the singular vectors which contain the greatest absolute
+        # value component with negative sign
+        flip_svd(U, Vh)
 
-            # A = X'X --> A = USVh --> X'XV = US
-            U, S, Vh = np.linalg.svd(A, full_matrices=False)
-            # to obtain a deterministic result flip the sign of the eigenvectors which contain the greatest absolute
-            # value component with negative sign
-            flip_svd(U, Vh)
+        #self.principal_components = U[:, :self.num_components].T
+        self.principal_components = Vh[:self.num_components]
+        singular_values_ = S.copy()
 
-            self.principal_components = U[:, :self.num_components].T
-            singular_values_ = S.copy()
+        if self.verbose:
+            print(f"- Compute SVD:  X = U S Vt")
+            print(f"- Dimensionality of the computed matrices")
+            print(f"\t- U       (left-singular vectors) : {U.shape[0]} x {U.shape[1]}")
 
-            if self.verbose:
-                print(f"- Compute SVD of A = U S Vh")
-                print(f"- Dimensionality of the computed matrices")
-                print(f"\t- U       (left-singular vectors) : {U.shape[0]} x {U.shape[1]}")
+            print(f"\t- S       (singular values)       : {S.shape[0]} x {S.shape[0]}")
+                  #f"\n\t[np.linalg.svd returns only the diagonal of S]")
+            print(f"\t- V       (right-singular vectors): {Vh.shape[1]} x {Vh.shape[0]}")
+            print("-" * 50)
+            print(f"- Select the first {self.num_components} right-singular vectors of X with "
+                  f"\nthe largest singular values as principal components")
+            print("-" * 50)
 
-                print(f"\t- diag(S) (singular values)       : {S.shape[0]} x 1 ")
-                      #f"\n\t[np.linalg.svd returns only the diagonal of S]")
-                print(f"\t- Vh      (right-singular vectors): {Vh.shape[0]} x {Vh.shape[1]}")
-                print("-" * 50)
-                print(f"- Select the first {self.num_components} left-singular vectors of A with "
-                      f"\nthe largest singular values as principal components of X")
-                print("-" * 50)
-
-        else:
-            # num_features >>> num_samples
-            # case B
-            B = X_ @ X_.T
-            if self.verbose:
-                print("Case B (num_features > num_samples) :")
-                print(f" - Compute B = XX'")
-                print(f" - XX' shape : ({X_.shape[0]} x {X_.shape[1]}) * ({X_.shape[1]} x {X_.shape[0]})")
-                print(f"             :  {B.shape[0]} x {B.shape[1]}")
-                print("-" * 50)
-
-            # B = XX' --> B = USVh --> XX' = USVh
-            U, S, Vh = np.linalg.svd(B)
-            flip_svd(U, Vh, U_based=False)
-
-            product = X_.T @ Vh[:self.num_components]  # take the rows
-            self.principal_components = product / np.linalg.norm(product, axis=0)
-            singular_values_ = S.copy()
-
-            if self.verbose:
-                print(f"- Compute SVD of B = U S Vh")
-                print(f"- Dimensionality of the computed matrices")
-                print(f"\t- U       (left-singular vectors) : {U.shape[0]} x {U.shape[1]}")
-
-                print(f"\t- diag(S) (singular values)       : {S.shape[0]} x 1 ")
-                # f"\n\t[np.linalg.svd returns only the diagonal of S]")
-                print(f"\t- Vh      (right-singular vectors): {Vh.shape[0]} x {Vh.shape[1]}")
-                print("-" * 50)
-                print(f"- Select the first {self.num_components} right-singular vectors of B with "
-                      f"\nthe largest singular values")
-                print(f"- Compute the principal components of X as:"
-                      f"\n u_i = (X' v_i)/ ||(X' v_i)|| for i in 1, ... , {self.num_components}")
-                print("-" * 50)
-
-        self.singular_values = np.sqrt(singular_values_[:self.num_components])
-        self.explained_variance = singular_values_ / (self.num_samples - 1)
+        #self.eigen_values = (singular_values_ ** 2)/(self.num_samples - 1)  #np.sqrt(singular_values_[:self.num_components])
+        self.explained_variance = (singular_values_ ** 2)/(self.num_samples - 1)
         self.explained_variance_ratio = self.explained_variance / self.explained_variance.sum()
 
         if self.verbose:
             print(f"- Summary of SVD results: ")
             print()
-            print(f"- Singular values:")
-            for i in range(singular_values_.shape[0]):
-                print(f"\t{i+1} - {np.sqrt(singular_values_[i])}")
+            print(f"- Eigen Values of Cov(X):")
+            for i in range(self.explained_variance.shape[0]):
+                print(f"\t{i+1} - {self.explained_variance[i]}")
             print()
             print(f"- Explained variance by each principal component")
             for i in range(self.explained_variance_ratio.shape[0]):
